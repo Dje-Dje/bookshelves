190af43ad478c8feda54583ebfd42f657369d231
ANDRE Jérôme <andre.jerome@protonmail.com>
2018-10-08 10:03:25 +0200

    bookshelves project - signup/in

--- bookshelves/src/app/app.component.ts
+++ bookshelves/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from "firebase";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bookshelves';
+
+  constructor() {
+    var config = {
+      apiKey: "AIzaSyAdiQRk1cXhRiao-FxL2092cuyfwYFtii4",
+      authDomain: "bookshelves-30dfe.firebaseapp.com",
+      databaseURL: "https://bookshelves-30dfe.firebaseio.com",
+      projectId: "bookshelves-30dfe",
+      storageBucket: "bookshelves-30dfe.appspot.com",
+      messagingSenderId: "169187650576"
+    };
+    firebase.initializeApp(config);
+  }
+
 }
